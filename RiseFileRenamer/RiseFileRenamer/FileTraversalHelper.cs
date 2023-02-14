f38867798cb055f013a54bdd9adc06c369b90951
CouchCmd <commoncasualcouchcommando@gmail.com>
2023-02-13 21:28:54 -0700

    this is phase one of using 本本子, Silvris, Ando, Asterisk, MHVuze mod list to automatically rename mod files to match the base game.  it will delete all files that do not exist in the list.

---
+++ RiseFileRenamer/RiseFileRenamer/FileTraversalHelper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+
+namespace RiseFileRenamer;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public static class FileTraversalHelper
+{
+	public static void FileTraversal (string baseLocation, List<string> validData, string origin)
+	{
+		var files = Directory.GetFiles(baseLocation);
+		var directories = Directory.GetDirectories(baseLocation);
+
+		foreach ( var file in files )
+		{
+			CheckFileAndRename(file, validData, origin);
+		}
+
+		// First logic rename all stm to msg
+		if (directories.Any(x => x.Equals($"{baseLocation}\\stm", StringComparison.OrdinalIgnoreCase)))
+		{
+			var stmLocaiton = directories.First(x => x.Equals($"{baseLocation}\\stm", StringComparison.OrdinalIgnoreCase));
+
+			Directory.Move(stmLocaiton, $"{baseLocation}\\MSG");
+
+			directories = Directory.GetDirectories(baseLocation);
+        }
+
+		// recurse dang it
+		foreach ( var directory in directories )
+		{
+			FileTraversal(directory, validData, origin);
+		}
+
+        files = Directory.GetFiles(baseLocation);
+        directories = Directory.GetDirectories(baseLocation);
+
+		if (directories.Count() == 0 && files.Count() == 0) 
+		{
+			Directory.Delete(baseLocation);
+		}
+    }
+
+	public static void CheckFileAndRename(string fileName, List<string> validData, string origin)
+    {
+		if (!validData.Any(x => x.Equals(fileName, StringComparison.OrdinalIgnoreCase)))
+		{
+			var validFiles = new List<string>();
+			var renameFile = string.Empty;
+			var drillDownName = fileName.Replace(origin, string.Empty);
+            while (!validFiles.Any())
+            {
+                var newValue = "\\" + drillDownName.Split('\\').Last();
+
+                drillDownName = drillDownName.Replace(newValue, string.Empty);
+
+				renameFile = String.IsNullOrEmpty(renameFile) ? newValue : newValue + renameFile;
+                validFiles = validData.Where(x => x.Contains(drillDownName, StringComparison.OrdinalIgnoreCase)).ToList();
+            }
+
+			bool matched = false;
+			foreach ( var file in validFiles)
+			{
+				
+				var validName = file.Remove(0, drillDownName.Length);
+				var validComponents = validName.Split('.').ToList();
+				var modComponents = renameFile.Split('.').ToList();
+
+                var correctName = MatchAndReplaceComponents(validComponents, modComponents);
+
+				if (!String.IsNullOrEmpty(correctName))
+				{
+					renameFile = correctName;
+					matched = true;
+					break;
+                }
+			}
+
+			if (matched)
+			{
+				File.Move(fileName, origin + drillDownName  + renameFile);
+			}
+			else
+			{
+				File.Delete(fileName);
+			}
+
+		}
+	}
+
+	public static string MatchAndReplaceComponents(List<string> validComponents, List<string> modComponents)
+	{
+		bool different = false;
+		int location = 0;
+
+		string match = string.Empty;
+		do
+		{
+			if (validComponents[location].Equals(modComponents[location], StringComparison.OrdinalIgnoreCase))
+			{
+				match = match + validComponents[location] + '.';
+				location++;
+			}
+			else
+			{
+				different = true;
+			}
+		} while (!different && validComponents.Count > location);
+
+		if (location < validComponents.Count - 1)
+		{
+			return string.Empty;
+		}
+
+		if (location == validComponents.Count)
+		{
+			return match.Remove(match.Length - 1, 1);
+        }
+
+
+		return match + validComponents[location];
+	}
+}
---
+++ RiseFileRenamer/RiseFileRenamer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using RiseFileRenamer;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please make sure to put mhrisePC_GamePass.list in same folder as executable");
+var myLocation = Directory.GetCurrentDirectory();
+
+if (!File.Exists($"{myLocation}\\mhrisePC_GamePass.list"))
+{
+    Console.WriteLine($"\nmhrisePC_GamePass.lis missing at \n{myLocation}");
+    Thread.Sleep(5000);
+}
+
+Console.WriteLine("\nPlease enter folder location of mod you would like to update for gamepass or exit to quite");
+var modLocation = Console.ReadLine();
+
+while (string.IsNullOrEmpty(modLocation) || modLocation.Equals("exit", StringComparison.OrdinalIgnoreCase) || !Directory.Exists(modLocation) || !modLocation.EndsWith("natives", StringComparison.OrdinalIgnoreCase))
+{
+    Console.WriteLine($"\nPlease enter a valid location (must start at the natives folder) or exit");
+    modLocation = Console.ReadLine();
+}
+
+if (modLocation.Equals("exit", StringComparison.OrdinalIgnoreCase))
+    System.Environment.Exit(0);
+
+var locations = await File.ReadAllLinesAsync($"{myLocation}\\mhrisePC_GamePass.list");
+
+FileTraversalHelper.FileTraversal(modLocation, locations.ToList(), modLocation.Replace("natives", string.Empty));
+
+Console.WriteLine("Completed Conversion");
+Thread.Sleep(5000);
